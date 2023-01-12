ee4f9297b8d75683c0a1dc6559a758cff1f8b1de
Duque <107492575+JaoDuque@users.noreply.github.com>
2023-01-12 10:12:27 -0300

    Update diagonal.php

--- php-novo/avançado/diagonal.php
+++ php-novo/avançado/diagonal.php
@@ -6,11 +6,30 @@ $n = (int)fgets(STDIN);
 
 for ($i = 0; $i < $m; $i++){
     for ($j = 0; $j < $n; $j++){
-       
-        $array[$i][$j] = (int)fgets(STDIN);
-       
-        if($array = $array[$i][$j]){
-            echo $array . PHP_EOL;
+       $array[$i][$j] = (int)fgets(STDIN);
+       }
+}
+
+for ($i = 0; $i < $m; $i++){
+    for ($j = 0; $j < $n; $j++){
+       echo $array[$i][$j];
+    }
+    echo PHP_EOL;
+}
+
+for ($i = 0; $i < $m; $i++){
+    for ($j = 0; $j < $n; $j++){
+       if ($i == $j){
+        echo $array[$i][$j];
+       }
+    }
+    echo PHP_EOL;
+}
+for ($i = 0; $i < $m; $i++){
+    for ($j = 0; $j < $n; $j++){
+        if ($j==$n-1-$i){
+            echo $array[$i][$j];
         }
     }
+    echo PHP_EOL;
 }
